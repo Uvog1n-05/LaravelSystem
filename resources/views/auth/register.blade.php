3edbb7c38396ad0f1e9ca72b552cc198e52172f7
Uvog1n-05 <graciosajohnwick03@gmail.com>
2025-10-20 21:02:01 +0800

    Fix card hover effect and update error message list styling in registration form

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -55,7 +55,7 @@
                 <button type="submit" class="btn">Create Account</button>
 
                 @if ($errors->any())
-                    <ul>
+                    <ul >
                         @foreach ($errors->all() as $error)
                             <li>{{ $error }}</li>
                         @endforeach
